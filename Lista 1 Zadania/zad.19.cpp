641809e6c46a73c2f8c9d7ea7349018aa0012b78
pawelpustula <32542099+pawelpustula@users.noreply.github.com>
2018-11-16 20:43:44 +0100

    Add files via upload

---
+++ Lista 1 Zadania/zad.19.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int zamien(char[]);
+
+int main()
+{
+	char liczba[1024] = {};
+	cout << "Prosze podac liczbe do zamiany: ";
+	cin >> liczba;
+	cout << "Po zamianie: " << zamien(liczba) << "\n";;
+
+	system("pause");
+	return 0;
+}
+
+int zamien(char liczba[])
+{
+	int wynik = 0, znak = 1, i = 0;
+
+	if (liczba[0] == '-')
+	{
+		znak = -1;
+		i++;
+	}
+	
+	while (liczba[i] >= 48 && liczba[i] <= 57 && i < strlen(liczba))
+	{
+		wynik = wynik * 10 + liczba[i] - '0';
+		i++;
+	}
+
+	return znak*wynik;
+}
